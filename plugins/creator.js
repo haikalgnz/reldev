74276a251624b15934186434a62d3c8d908bdf3e
haikalgnz <haikalofc8@gmail.com>
2021-11-25 15:52:03 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = function (m) {
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 let fs = require('fs')
 let name = this.contacts[m.sender].notify
-let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '6289696454872@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   
   
